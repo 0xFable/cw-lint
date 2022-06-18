205fc31095526fee1faadec6ded1c1d5daff9462
0xFable <0xfable@protonmail.com>
2022-06-18 18:06:03 +0100

    feat(experiment): Initial CanonAddr check - understanding the checks

--- src/lib.rs
+++ src/lib.rs
@@ -19,7 +19,17 @@ extern crate rustc_target;
 extern crate rustc_trait_selection;
 extern crate rustc_typeck;
 
-use rustc_lint::LateLintPass;
+use rustc_lint::{LateLintPass,LateContext, LateContextAndPass};
+use rustc_hir::{self as hir, def_id::DefId};
+use clippy_utils::diagnostics::span_lint_and_help;
+use clippy_utils::ty::{is_type_diagnostic_item, is_type_lang_item};
+use cosmwasm_std::{CanonicalAddr};
+pub fn is_canon_addr_call(cx: &LateContext<'_>, fn_def_id: DefId) -> bool {
+    match_def_path(cx, fn_def_id, &CANONADDRCALL)
+}
+
+pub const CANONADDRCALL: [&str; 2] = ["cosmwasm_std", "CanonicalAddr"];
+
 
 dylint_linting::declare_late_lint! {
     /// **What it does:**
@@ -45,7 +55,33 @@ dylint_linting::declare_late_lint! {
 impl<'tcx> LateLintPass<'tcx> for FillMeIn {
     // A list of things you might check can be found here:
     // https://doc.rust-lang.org/stable/nightly-rustc/rustc_lint/trait.LateLintPass.html
+
 }
+
+impl LateLintPass<'_> for MyStructLint {
+    fn check_expr(&mut self, cx: &LateContext<'_>, expr: &Expr<'_>) {
+        // Getting the expression type
+        let ty = cx.typeck_results().expr_ty(expr);
+
+        // 1. Using diagnostic items
+        // The last argument is the diagnostic item to check for
+        if is_canon_addr_call(cx, ty) {
+            // The type is an `Option`
+            span_lint_and_help(
+                cx,
+                FILL_ME_IN,
+                hir_ty.span,
+                "you seem to be using a `CanonicalAddr`! This is often not needed and Addr should be used instead.",
+                None,
+                "an `Addr` might work",
+            );
+            true
+        } else {
+            false
+        }
+    }
+}
+
 
 #[test]
 fn ui() {
